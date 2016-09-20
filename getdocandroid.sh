69c2ea98b3e8b752a742f2c260ba55b07cee0812
RWebRTC <rwebrtc@gmail.com>
2016-09-20 21:22:24 +0800

    update getdocandroid.sh

--- getdocandroid.sh
+++ getdocandroid.sh
@@ -1,7 +1,26 @@
+#!/bin/sh
 echo "get doc begin"
 git checkout gh-pages 
 rm -rf android-javadoc/
 cp -r ~/AndroidStudioProjects/Rlib/rlib/build/docs/javadoc/ android-javadoc
 git add .
 git status
-echo "please commit, push and checkout develop"
+
+if [ "$1" =  "" ]; then 
+    echo "please commit, push and checkout develop"
+else
+    git commit -m "$1"
+    echo "git commit finish"
+    if [ "$2" = "" ]; then
+        echo "git not push"
+        git checkout develop
+        git branch
+    else
+        if [ "$2" = "y" ]; then
+            git push origin
+            echo "git push finish"
+            git checkout develop
+            git branch
+        fi
+    fi
+fi
