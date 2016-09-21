7e18d314f9e0aad8e3aa43eb4b0105671778ba40
RWebRTC <rwebrtc@gmail.com>
2016-09-21 17:45:53 +0800

    update getSource.sh

--- getSource.sh
+++ getSource.sh
@@ -1,5 +1,14 @@
+echo "-----Android begin-----"
+echo "clean android/ dir"
 rm -rf android/src/
 rm -rf android/src/RALib.jar
 cp -r ~/AndroidStudioProjects/Rlib/ ./android/src
+echo "cp file into android/"
 cp android/src/rlib/build/libs/RALib.jar ./android/
+cp android/src/build.gradle-alib android/src/build.gradle
+cp android/src/rlib/build.gradle-alib android/src/rlib/build.gradle
+echo "finish getSource"
+echo "-----Android finish-----"
+
+git status
 
