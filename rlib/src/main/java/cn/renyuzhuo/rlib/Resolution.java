885cfdb0a3432049283a3ac5a74c16f299d2074a
RWebRTC <rwebrtc@gmail.com>
2016-09-23 09:56:39 +0800

    fix bug

---
+++ rlib/src/main/java/cn/renyuzhuo/rlib/Resolution.java
@@ -0,0 +1,61 @@
+package cn.renyuzhuo.rlib;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+/**
+ * Created by renyuzhuo on 16-9-21.
+ * 单位转换库
+ */
+public class Resolution {
+
+    /**
+     * dp转换成px
+     *
+     * @param context  Context
+     * @param dipValue dip
+     * @return pix, context为空时，返回dipValue
+     */
+    public static float dipToPixels(Context context, float dipValue) {
+        if (context == null) {
+            return dipValue;
+        }
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dipValue, context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * dp转换成px
+     *
+     * @param context  Context
+     * @param dipValue dip
+     * @return pix, context为空时，返回dipValue
+     */
+    public static float dipToPixels(Context context, int dipValue) {
+        if (context == null) {
+            return dipValue;
+        }
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dipValue, context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * dp转换成px
+     *
+     * @param context  Context
+     * @param dipValue dip
+     * @return pix, context为空时，返回dipValue
+     */
+    public static int dipToPixelsInt(Context context, float dipValue) {
+        return (int) dipToPixels(context, dipValue);
+    }
+
+    /**
+     * dp转换成px
+     *
+     * @param context  Context
+     * @param dipValue dip
+     * @return pix, context为空时，返回dipValue
+     */
+    public static int dipToPixelsInt(Context context, int dipValue) {
+        return (int) dipToPixels(context, dipValue);
+    }
+}
