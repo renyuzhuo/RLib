1d39816105bad5afd3c51842488d9c6362407a6e
RWebRTC <rwebrtc@gmail.com>
2016-10-27 16:00:42 +0800

    update rlog and rjson

--- android/src/app/src/main/java/cn/renyuzhuo/MainActivity.java
+++ android/src/app/src/main/java/cn/renyuzhuo/MainActivity.java
@@ -3,16 +3,11 @@ package cn.renyuzhuo;
 import android.app.Activity;
 import android.os.Bundle;
 
-import cn.renyuzhuo.rlog.rlog;
-
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        rlog.d("in MainActivity");
-
     }
 }
