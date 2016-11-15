7320d7ee881703edea824678983b3106878a011b
RWebRTC <rwebrtc@gmail.com>
2016-11-15 16:11:39 +0800

    update rlog

--- rlog/src/main/java/cn/renyuzhuo/rlog/rlog.java
+++ rlog/src/main/java/cn/renyuzhuo/rlog/rlog.java
@@ -2,404 +2,95 @@ package cn.renyuzhuo.rlog;
 
 import android.util.Log;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.util.Random;
 
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-
 /**
- * 日志输出工具类
+ * rlog.getSettings().logLevel(BuildConfig.DEBUG ? LogLevel.FULL : LogLevel.NONE);
  * <p>
- * i()系列方法输出info信息
- * <p>
- * d()系列方法输出debug信息
- * <p>
- * w()系列方法输出warn信息
- * <p>
- * e()系列方法输出error信息
+ * Thanks https://github.com/orhanobut/logger
  */
-public class rlog {
+public final class rlog {
 
-    private static String TAG = "rlog";
-    private static String TAG_INFO = TAG + ".i";
-    private static String TAG_DEBUG = TAG + ".d";
-    private static String TAG_WARN = TAG + ".w";
-    private static String TAG_ERROR = TAG + ".e";
+    private static Printer printer = new LoggerPrinter();
 
-    private static int debugLevel = DEBUG_LEVEL.debug;
+    public static Printer t(String tag) {
+        return printer.t(tag, printer.getSettings().getMethodCount());
+    }
 
-    // Log.info
-    public static void i(String tag, String message) {
-        switch (debugLevel) {
-            case DEBUG_LEVEL.info: {
-                Log.i(tag, message);
-                break;
-            }
+    public static Printer t(int methodCount) {
+        return printer.t(null, methodCount);
+    }
+
+    public static Printer t(String tag, int methodCount) {
+        return printer.t(tag, methodCount);
+    }
+
+    public static void log(int priority, String tag, String message, Throwable throwable) {
+        printer.log(priority, tag, message, throwable);
+    }
+
+    public static void d(String message, Object... args) {
+        printer.d(message, args);
+    }
+
+    public static void d(Object object) {
+        printer.d(object);
+    }
+
+    public static void e(String message, Object... args) {
+        printer.e(null, message, args);
+    }
+
+    public static void e(Throwable throwable, String message, Object... args) {
+        printer.e(throwable, message, args);
+    }
+
+    public static void i(String message, Object... args) {
+        printer.i(message, args);
+    }
+
+    public static void v(String message, Object... args) {
+        printer.v(message, args);
+    }
+
+    public static void w(String message, Object... args) {
+        printer.w(message, args);
+    }
+
+    public static void wtf(String message, Object... args) {
+        printer.wtf(message, args);
+    }
+
+    public static void log(Object o) {
+        if (settings.getLogLevel() == LogLevel.NONE) {
+            return;
         }
-    }
-
-    public static void i(String message) {
-        i(TAG_INFO, message);
-    }
-
-    public static void i(Object o) {
         if (o == null) {
-            e("object == null");
-            return;
-        }
-        i(o.toString());
-    }
-
-    // Log.debug
-    public static void d(String tag, String message) {
-        if (message == null) {
-            d("message == null");
-            return;
-        }
-        if (message.length() == 0) {
-            d("message.length == 0");
-            return;
-        }
-        switch (debugLevel) {
-            case DEBUG_LEVEL.info:
-            case DEBUG_LEVEL.debug: {
-                Log.d(tag, message);
-            }
-        }
-    }
-
-    public static void d(String message) {
-        d(TAG_DEBUG, message);
-    }
-
-    /**
-     * 输出空行------
-     */
-    public static void d() {
-        d("------------------------------");
-    }
-
-    /**
-     * debug级别语句块开始
-     */
-    private static void dbegin(String s) {
-        d("↓↓↓↓↓" + s + "↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓");
-    }
-
-    /**
-     * debug级别语句块开始
-     */
-    public static void dbegin() {
-        d("↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓");
-    }
-
-    /**
-     * debug级别语句块结束
-     */
-    private static void dend(String s) {
-        d("↑↑↑↑↑" + s + "↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑");
-    }
-
-    /**
-     * debug级别语句块结束
-     */
-    public static void dend() {
-        d("↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑");
-    }
-
-    public static void d(char c) {
-        d(TAG_DEBUG + ".char", String.valueOf(c));
-    }
-
-    public static void d(int numInt) {
-        d(TAG_DEBUG + ".numInt", String.valueOf(numInt));
-    }
-
-    public static void d(long numLong) {
-        d(TAG_DEBUG + ".numLong", String.valueOf(numLong));
-    }
-
-    public static void d(float numFloat) {
-        d(TAG_DEBUG + ".numFloat", String.valueOf(numFloat));
-    }
-
-    public static void d(double numDouble) {
-        d(TAG_DEBUG + ".numDouble", String.valueOf(numDouble));
-    }
-
-    public static void d(Object o) {
-        if (o == null) {
-            e("object == null");
-            return;
-        }
-        d(TAG_DEBUG, o.toString());
-    }
-
-    public static void d(Object[] objects) {
-        if (objects == null) {
-            e("array == null");
-            return;
-        }
-
-        if (objects.length == 0) {
-            d(TAG_DEBUG + ".array", "[]");
-            return;
-        }
-
-        String temp = "[";
-        for (Object o : objects) {
-            temp += o + ", ";
-        }
-        // remove the last','
-        temp = temp.substring(0, temp.length() - 2);
-        temp += "]";
-
-        d(TAG_DEBUG + ".array", temp);
-    }
-
-    // warn
-    public static void w(String tag, String message) {
-        switch (debugLevel) {
-            case DEBUG_LEVEL.info:
-            case DEBUG_LEVEL.debug:
-            case DEBUG_LEVEL.warn: {
-                Log.w(tag, message);
-            }
-        }
-    }
-
-    // warn
-    public static void w(String message) {
-        w(TAG_WARN, message);
-    }
-
-    // error
-    public static void e(String tag, String message) {
-        switch (debugLevel) {
-            case DEBUG_LEVEL.info:
-            case DEBUG_LEVEL.debug:
-            case DEBUG_LEVEL.warn:
-            case DEBUG_LEVEL.error: {
-                Log.e(tag, message);
-            }
-        }
-    }
-
-    public static void e(String message) {
-        e(TAG_ERROR, message);
-    }
-
-    /**
-     * 错误语句块开始
-     */
-    private static void ebegin(String s) {
-        e("↓↓↓↓↓" + s + "↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓");
-    }
-
-    /**
-     * 错误语句快开始
-     */
-    public static void ebegin() {
-        e("↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓");
-    }
-
-    /**
-     * 错误语句块结束
-     */
-    private static void eend(String s) {
-        e("↑↑↑↑↑" + s + "↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑");
-    }
-
-    /**
-     * 错误语句块结束
-     */
-    public static void eend() {
-        e("↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑");
-    }
-
-    // objects
-    private static Object object;
-
-    /**
-     * 分别输出objects
-     *
-     * @param objects objects
-     */
-    public static void objects(Object... objects) {
-        if (objects == null || objects.length == 0) {
-            d();
-            return;
+            Log.d(settings.getTAG(), "null");
         } else {
-            dbegin("Objects begin");
-            for (int i = 0; i < objects.length; i++) {
-                object = objects[i];
-                if (object == null) {
-                    d("args[" + i + "] == null");
-                } else {
-                    d("args[" + i + "] == " + String.valueOf(object));
-                }
-            }
-            dend("Objects end");
+            Log.d(settings.getTAG(), o.toString());
         }
     }
 
     /**
-     * 以Debug级别输出json
+     * Formats the json content and print it
+     *
+     * @param json the json content
      */
     public static void json(String json) {
-        json(DEBUG_LEVEL.debug, json);
+        printer.json(json);
     }
 
     /**
-     * 以debug_level级别输出json
+     * Formats the json content and print it
      *
-     * @param debug_level 日志级别
-     * @param json        json字符串
-     */
-    public static void json(int debug_level, String json) {
-        if (json == null) {
-            e("json == null");
-            return;
-        }
-        if (json.length() == 0) {
-            e("json.length() == 0");
-            return;
-        }
-        // https://github.com/orhanobut/logger
-        json = json.trim();
-        int JSON_INDENT = 2;
-
-        String logString = null;
-        try {
-            if (json.startsWith("{")) {
-                JSONObject jsonObject = new JSONObject(json);
-                logString = jsonObject.toString(JSON_INDENT);
-            } else if (json.startsWith("[")) {
-                JSONArray jsonArray = new JSONArray(json);
-                logString = jsonArray.toString(JSON_INDENT);
-            }
-        } catch (JSONException e) {
-            e("json err:" + json);
-        }
-        if (logString == null) {
-            e("json -> null. Json formate err.");
-            return;
-        }
-
-        switch (debug_level) {
-            case DEBUG_LEVEL.info: {
-                i(TAG_INFO + ".json", logString);
-                break;
-            }
-            case DEBUG_LEVEL.debug: {
-                d(TAG_INFO + ".json", logString);
-                break;
-            }
-            case DEBUG_LEVEL.warn: {
-                w(TAG_INFO + ".json", logString);
-                break;
-            }
-            case DEBUG_LEVEL.error: {
-                e(TAG_INFO + ".json", logString);
-                break;
-            }
-            default: {
-                break;
-            }
-        }
-    }
-
-    /**
-     * 以debug级别输出xml
+     * @param xml the xml content
      */
     public static void xml(String xml) {
-        xml(DEBUG_LEVEL.debug, xml);
+        printer.xml(xml);
     }
 
-    /**
-     * 以debug_level级别输出xml
-     *
-     * @param debug_level 日志级别
-     * @param xml         xml字符串
-     */
-    public static void xml(int debug_level, String xml) {
-        if (xml == null) {
-            e("xml == null");
-        } else if (xml.length() == 0) {
-            e("xml.length() == 0");
-        } else {
-            try {
-                // https://github.com/orhanobut/logger
-                Source xmlInput = new StreamSource(new StringReader(xml));
-                StreamResult xmlOutput = new StreamResult(new StringWriter());
-                Transformer transformer = TransformerFactory.newInstance().newTransformer();
-                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-                transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-                transformer.transform(xmlInput, xmlOutput);
-                switch (debug_level) {
-                    case DEBUG_LEVEL.info: {
-                        i(TAG_INFO + ".xml", xmlOutput.getWriter().toString().replaceFirst(">", ">\n"));
-                        break;
-                    }
-                    case DEBUG_LEVEL.debug: {
-                        d(TAG_INFO + ".xml", xmlOutput.getWriter().toString().replaceFirst(">", ">\n"));
-                        break;
-                    }
-                    case DEBUG_LEVEL.warn: {
-                        w(TAG_INFO + ".xml", xmlOutput.getWriter().toString().replaceFirst(">", ">\n"));
-                        break;
-                    }
-                    case DEBUG_LEVEL.error: {
-                        e(TAG_INFO + ".xml", xmlOutput.getWriter().toString().replaceFirst(">", ">\n"));
-                        break;
-                    }
-                    default: {
-                        break;
-                    }
-                }
-
-            } catch (TransformerException e) {
-                e("xml err:" + xml);
-            }
-        }
-    }
-
-    /**
-     * 日志级别
-     */
-    public interface DEBUG_LEVEL {
-        /**
-         * info级别
-         */
-        int info = 0,
-        /**
-         * debug级别
-         */
-        debug = 1,
-        /**
-         * warn级别
-         */
-        warn = 2,
-        /**
-         * error级别
-         */
-        error = 3,
-        /**
-         * 不输出日志
-         */
-        none = 7;
-    }
+    private static final Settings settings = Settings.getInstance();
 
     /**
      * 设置日志输出tag
@@ -407,11 +98,7 @@ public class rlog {
      * @param tag TAG
      */
     public static void setTAG(String tag) {
-        TAG = tag;
-        TAG_INFO = tag + ".i";
-        TAG_DEBUG = tag + ".d";
-        TAG_WARN = tag + ".w";
-        TAG_ERROR = tag + ".e";
+        Settings.getInstance().setTAG(tag);
     }
 
     /**
@@ -437,12 +124,9 @@ public class rlog {
         }
     }
 
-    /**
-     * 设置日志输出级别
-     *
-     * @param level 日志级别
-     */
-    public static void setDebugLever(int level) {
-        debugLevel = level;
+    public static Settings getSettings() {
+        return Settings.getInstance();
     }
+
 }
+
